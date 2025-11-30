d33c53f699d318e6ccd5bc2e4d1601e97d38eee0
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-11-30 11:18:08 -0800

    Added glow to gun so it's not black at the windows Added Aim Down Sight (ads) on right click

--- scriptsnscenes/player/camera.gd
+++ scriptsnscenes/player/camera.gd
@@ -5,8 +5,10 @@ extends Camera3D
 @export var gun : MeshInstance3D
 var curr_dist : float = 100.
 var target_dist : float = -1.
-const MIN_TARGET_DIST : float = 1.5
+@export var MIN_TARGET_DIST : float = 1.5
+@export var fov_z_offset : float = 0.013
 var og_marker : Vector3
+var is_aiming := false
 
 var can_shoot := true
 
@@ -23,9 +25,10 @@ func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("l_click"): 
 		if raycast.is_colliding(): 
 			_on_hit()
-		pass
 	if Input.is_action_just_pressed("r_click"): 
-		pass
+		is_aiming = true
+	elif Input.is_action_just_released("r_click"):
+		is_aiming = false
 		
 func _on_hit():
 	if not raycast.is_colliding(): return
@@ -39,11 +42,11 @@ func _on_hit():
 
 
 func _process(delta: float) -> void:
-	gun_marker.position = og_marker + Vector3(
-		0, 
-		0, 
-		-(75-player.camera.fov) * 0.015
-	)
+	if is_aiming:
+		gun_marker.position.x *= .9
+	else:
+		gun_marker.position = lerp(gun_marker.position, og_marker, delta*3)
+	gun_marker.position.z = og_marker.z + lerpf(0, fov_z_offset, 75-player.camera.fov)
 	
 	if raycast.is_colliding():
 		target_dist = raycast.get_collision_point().distance_to(raycast.global_position)
