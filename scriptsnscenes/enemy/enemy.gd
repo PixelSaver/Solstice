1bef9d12fd7c678dabc2ae8620ee258f82600014
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-11-30 23:36:36 -0800

    Added wendigo animations when walking and idling

--- scriptsnscenes/enemy/enemy.gd
+++ scriptsnscenes/enemy/enemy.gd
@@ -1,27 +1,29 @@
-extends RigidBody3D
+extends StaticBody3D
 class_name Enemy
 
+signal reached_point(idx:int)
+@export var move_speed : float = 2.
+@export var anim_player : AnimationPlayer
 var health_component : HealthComponent
 var movement_points : EnemyMovementPoints
 var move_idx : int = 0
 var points : Array[Marker3D]
 var current_target_position : Vector3
+var is_moving := false
 
 func _ready() -> void:
 	health_component = get_tree().get_first_node_in_group("health_component") 
 	health_component.died.connect(_on_death)
-	#DEBUG
-	await get_tree().create_timer(1).timeout
-	move_to_next_point()
-	await get_tree().create_timer(1).timeout
-	move_to_next_point()
-	await get_tree().create_timer(1).timeout
-	move_to_next_point()
+	reached_point.connect(_on_point_reached)
 
-func set_movement_points(given_movement_points:EnemyMovementPoints):
+func set_movement_points(given_movement_points : EnemyMovementPoints):
 	movement_points = given_movement_points
 	points = given_movement_points.points_list
-	_update_pos()
+	_begin_movement()
+
+func _begin_movement():
+	self.global_position = points[move_idx].global_position
+	move_to_next_point()
 
 func move_to_next_point():
 	move_idx += 1
@@ -29,16 +31,48 @@ func move_to_next_point():
 
 func _update_pos():
 	if move_idx < points.size():
-		self.global_position = points[move_idx].global_position
-	elif move_idx >= points.size():
-		var t = create_tween().set_ease(Tween.EASE_IN)
+		current_target_position = points[move_idx].global_position
+	#elif move_idx >= points.size():
+		## End of movement points
+		#var t = create_tween().set_ease(Tween.EASE_IN)
 		#t.tween_property(self, "global_position", Global.player.global_position, 4)
 		#await get_tree().create_timer(3).timeout
 		#Global.death.emit()
 
-func _process(_delta: float) -> void:
+func _on_point_reached(idx:int):
+	print("idx: %s" % idx)
+	if idx < points.size()-1:
+		await get_tree().create_timer(randfn(1, 2)).timeout
+		move_to_next_point()
+	elif idx == points.size()-1:
+		anim_player.speed_scale = 1.
+		anim_player.play("idle")
+		self.look_at(Global.player.global_position)
+		anim_player.animation_finished.connect(
+			func(_anim_name:String):
+				anim_player.play("walk")
+				current_target_position = Global.player.global_position
+				move_to_next_point()
+				move_speed *= 5
+				, CONNECT_ONE_SHOT
+			)
+	else:
+		Global.death.emit()
+
+func _process(delta: float) -> void:
 	if not Global.player: return
-	self.look_at(Global.player.global_position)
+	is_moving = false if current_target_position.is_equal_approx(self.global_position) else true
+	if is_moving and anim_player.current_animation == "walk":
+		self.look_at(current_target_position)
+		self.global_position = self.global_position.move_toward(current_target_position, delta * move_speed)
+		anim_player.speed_scale = 1.
+		if current_target_position.is_equal_approx(\
+			self.global_position):
+			is_moving = false
+			reached_point.emit(move_idx)
+	elif anim_player.current_animation == "walk":
+		anim_player.speed_scale = 0.
+		self.look_at(Global.player.global_position)
 
 func _on_death():
 	queue_free()
--- scriptsnscenes/enemy/enemy_manager.gd
+++ scriptsnscenes/enemy/enemy_manager.gd
@@ -12,7 +12,7 @@ func enemy_spawn_loop():
 	print("spawned")
 	inst_enemy()
 	await get_tree().create_timer(5 + randf_range(-2,2)).timeout
-	enemy_spawn_loop()
+	#enemy_spawn_loop()
 
 func inst_enemy():
 	var enemy = ENEMY_SCENE.instantiate() as Enemy
